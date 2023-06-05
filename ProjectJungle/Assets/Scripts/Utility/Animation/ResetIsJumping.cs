6a0408ecfd3b7ad0bcbd5406b947281c4d21298d
IzzyDev <kishoreishaan@gmail.com>
2023-06-05 19:06:25 -0400

    Third Person Controller done
    No funky movement yet

---
+++ ProjectJungle/Assets/Scripts/Utility/Animation/ResetIsJumping.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ResetIsJumping : StateMachineBehaviour
+{
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.SetBool("isJumping", false);
+    }
+}
