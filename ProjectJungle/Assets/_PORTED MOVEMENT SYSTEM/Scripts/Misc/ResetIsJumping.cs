8e81ce0fecbf65c3ad2b20ecad6909f8cfc5cd49
Jegg's Laptop <avonroxahrebu23@gmail.com>
2024-03-20 11:24:46 -0400

    r

--- ProjectJungle/Assets/_PORTED MOVEMENT SYSTEM/Scripts/Misc/ResetIsJumping.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ResetIsJumping : StateMachineBehaviour
-{
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.SetBool("isJumping", false);
-    }
-}
