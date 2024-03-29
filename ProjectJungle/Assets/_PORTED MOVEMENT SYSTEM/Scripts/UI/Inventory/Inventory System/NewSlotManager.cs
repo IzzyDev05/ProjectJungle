02a69ed2f7aa6abf599aa573b7964cf8b16ca819
JL-40 <63485977+JL-40@users.noreply.github.com>
2024-03-29 00:08:56 -0400

    fixed animation issue with Interactions scripts???

--- ProjectJungle/Assets/_PORTED MOVEMENT SYSTEM/Scripts/UI/Inventory/Inventory System/NewSlotManager.cs
+++ ProjectJungle/Assets/_PORTED MOVEMENT SYSTEM/Scripts/UI/Inventory/Inventory System/NewSlotManager.cs
@@ -51,7 +51,7 @@ public class NewSlotManager : MonoBehaviour
             return false;
         }
 
-        Debug.Log(slotItem.name.Contains(pickedUpItem.name) ? true : false);
+        //Debug.Log(slotItem.name.Contains(pickedUpItem.name) ? true : false);
         return slotItem.name.Contains(pickedUpItem.name) ? true : false;
     }
 
